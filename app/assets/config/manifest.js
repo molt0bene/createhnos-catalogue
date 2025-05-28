1557ffe2ea4356d91f82a8d772753d12cc5bb929
molt0bene <alisar3@yandex.ru>
2025-05-28 13:16:30 +0300

    compile js in prod

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
+//= link application.js
+//= link assistant/init-assistant.js
+//= link assistant/dialog-handler.js
+//= link assistant/dialog-flow.js
