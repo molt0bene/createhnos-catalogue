91816f09376a9b2ce36e549879c43d54ec4f11b8
molt0bene <alisar3@yandex.ru>
2025-05-28 13:19:14 +0300

    fix

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
 //= link application.js
 //= link assistant/init-assistant.js
 //= link assistant/dialog-handler.js
